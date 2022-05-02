aea67baadd7aa8ce60f321fb8d00a1704ca199c1
tellikakneezus <Kadowling87@gmail.com>
2022-05-02 16:20:57 -0400

    Add files via upload

---
+++ ReplaceSpace/ReplaceSpace.java#
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class ReplaceSpace here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ReplaceSpace
+{
+    // instance variables - replace the example below with your own
+    private String str;
+
+    /**
+     * Constructor for objects of class ReplaceSpace
+     */
+    public ReplaceSpace(String str)
+    {
+        // initialise instance variables
+        this.str = str;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String replaceStr()
+    {
+        // put your code here
+        str.replaceAll(" ", ",");
+        
+        return str;
+    }
+}
---
+++ ReplaceSpace/Runner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Runner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Runner
+{
+    public static void main(String[] args){
+        ReplaceSpace rs = new ReplaceSpace("shlpfwpmq103 QCI1350951H 0I24660 D2-DV3-2/5 B200M1\nshlpfwpmq104 QCI1350951D 0I24661 D2-DV3-3/3 B200M1\nshlpnwpmq003 QCI1438A0KW 0I26060 D2-PN10-9/1 B200M2\nshlpnwpmq004 QCI1448A22R 0I26061 D2-PN10-10/1 B200M2\nshlpnwpmb003 QCI1413A0E4 0I25515 D2-PN8-5/6 B200M2\nshlpnwpmb004 QCI1427A2R6 0L23952 D2-PN8-6/1 B200M2\nshlpfwpmq201 QCI1413A0B1 0I25514 D2-DV1-8/5 B200M2\nshlpfwpmq202 QCI1414A42L 0I25509 D2-DV1-9/4 B200M2\nshlpnmqhb023 QCI135095II 0I24591 D2-PN25-4/3 B200M1\nshlpnmqhb024 QCI140206AD 0I24592 D2-PN25-5/3 B200M1\nshlpnmqhb001 QCI135095D7 0I24843 D2-PN28-1/3 B200M1\nshlpnmqhb002 QCI140202SC 0I25089 D2-PN28-2/3 B200M1\nshlpnmqhb003 QCI140202RE 0I25133 D2-PN28-3/5 B200M1\nshlpnmqhb004 QCI140202XX 0I25137 D2-PN28-4/3 B200M1\nshlpnmqhb005 QCI140103AR 0I25139 D2-PN28-5/3 B200M1\nshlpnmqhb006 QCI135201GG 0I25143 D2-PN28-6/3 B200M1\nshlpnmqhb007 QCI140202RC 0I25144 D2-PN28-1/4 B200M1\nshlpnmqhb008 QCI140202R5 0I25228 D2-PN28-2/4 B200M1 ");
+        
+        System.out.println(rs.replaceStr());
+
+            
+    }
+}
